9f87333c28f7d22eecb46eac014a734480d98d22
Rajamurugan <153163187+iamRajamurugan@users.noreply.github.com>
2024-05-22 11:18:55 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,10 @@
-#write a program to find the maximum of two numbers
+def find_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+max_number = find_max(a, b)
+print(f"The maximum of {a} and {b} is {max_number}.")
